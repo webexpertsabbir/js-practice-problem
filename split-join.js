79295feab5c8bed420e1ffaa345a608e4bcea661
Sabbir <webexpertsabbir@gmail.com>
2022-08-08 08:49:49 +0600

    s

--- split-join.js
+++ split-join.js
@@ -9,6 +9,17 @@ const jatioSongit = "Amar sonar Bangla ami tomay valobashi. cirodin tomar akash 
 // const parts = jatioSongit.split('.');
 // console.log(parts);
 
-const partial = jatioSongit.slice(5, 10);
-console.log(partial);
+const partial1 = jatioSongit.slice(5, 10);
+console.log(partial1);
 
+const partial2 = jatioSongit.substring(5, 10);
+
+console.log(partial2);
+
+const inLine = [
+    'Amar sonar Bangla ami tomay valobashi',
+    ' cirodin tomar akash ',
+    ' toamat batas'
+]
+const newSongit = inLine.join('. ')
+console.log(newSongit);
