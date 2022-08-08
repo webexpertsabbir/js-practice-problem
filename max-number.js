694c839398f8d4d68573876a89512c1aae534b2e
Sabbir <webexpertsabbir@gmail.com>
2022-08-08 10:33:31 +0600

    max number

---
+++ max-number.js
@@ -0,0 +1,16 @@
+
+function maxNmuber(numbers){
+    let lergest = numbers[0];
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element < lergest){
+            lergest = element
+        }
+    }
+    return lergest;
+}
+
+const heights = [126, 245, 30, 200, 300, 39];
+const talest = maxNmuber(heights);
+console.log(talest);
