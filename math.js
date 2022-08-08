40d988fb57f871a2f25e72dc251d319a6d094c89
Sabbir <webexpertsabbir@gmail.com>
2022-08-08 09:21:13 +0600

    math

---
+++ math.js
@@ -0,0 +1,19 @@
+const result = Math.pow(10, 5);
+// console.log(result);
+
+
+// const num1= 40;
+// const num2= 30;
+// const gap = Math.abs(num1 - num2);
+// if(gap < 5 ){
+//     console.log('You gyes can be friend')
+// }
+// else{
+//     console.log('not a friend');
+// }
+
+for (let i = 1; i <= 6; i++){
+    const random = Math.round(Math.random()*6);
+    console.log(random);
+}
+
