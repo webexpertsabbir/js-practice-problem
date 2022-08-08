6144723295b4732a25e91b004451018a9426902a
Sabbir <webexpertsabbir@gmail.com>
2022-08-08 08:41:20 +0600

    split join

---
+++ split-join.js
@@ -0,0 +1,14 @@
+const jatioSongit = "Amar sonar Bangla ami tomay valobashi. cirodin tomar akash . toamat batas";
+
+// const parts = jatioSongit.split(' ');
+// console.log(parts);
+
+// const parts = jatioSongit.split('');
+// console.log(parts);
+
+// const parts = jatioSongit.split('.');
+// console.log(parts);
+
+const partial = jatioSongit.slice(5, 10);
+console.log(partial);
+
