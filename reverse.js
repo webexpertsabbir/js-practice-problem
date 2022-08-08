26b284899874c09e1f21969184d486c981301a2a
Sabbir <webexpertsabbir@gmail.com>
2022-08-08 11:16:31 +0600

    reverse wor

--- reverse.js
+++ reverse.js
@@ -14,3 +14,16 @@ console.log(sreversed);
 
 // word reverse
 
+function reverseWord(str){
+    const words = str.split(' ');
+    const result = [];
+    for(let i = words.length-1; i >= 0; i--){
+        const emWord = words[i];
+        result.push(emWord);
+    }
+    const reversed1 = result.join(' ');
+    return reversed1;
+}
+const myString2 = 'i am a good boy';
+const finalResult = reverseWord(myString2);
+console.log(finalResult);
